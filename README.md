b513b454d5283c4e619ad55f959a2639cd8ddde1
Tiberiu Tofan <tiberiu.tofan@ing.com>
2021-05-26 17:48:21 +0300

    updated dependencies

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Kotlin, a language to rule DSLs
+## Kotlin, a Language to Rule DSLs
 
-Support slides and source code for my talk, "Kotlin, a language to rule DSLs"
+Kotlin is a great language for creating internal DSLs. Although it is statically typed, some of its features, such as type-safe builders, extension functions, operator overloading, implicit receivers, etc. make the creation of internal DSLs a rewarding experience. We will explore some existing DSLs, and look under the hood to see how we can tailor the same principles to our needs.
 
-To access the animated slides, please go to https://tibtof.github.io/kotlin-to-rule-dsls/
+The slides are available [here](https://tibtof.github.io/kotlin-to-rule-dsls/).
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
 repositories {
     mavenCentral()
-    jcenter()
 }
+
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.5.10"
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.10")
-    testImplementation("com.github.tomakehurst:wiremock:2.26.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
-    testImplementation("org.junit.platform:junit-platform-launcher:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.3")
 }
 
 tasks.test {
@@ -24,6 +21,6 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+        classpath(kotlin("gradle-plugin", version = "1.5.10"))
     }
 }
